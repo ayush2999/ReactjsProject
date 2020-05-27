3cc869ef330b35dd972ddcf3e9ca9dda1b54bd62
ayush <ayushsaxena723@gmail.com>
2020-05-27 21:25:10 +0530

    initializing

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -20,7 +20,7 @@ const mapStateToProps = state => {
 }
 
 
-class App extends Component {
+class Main extends Component {
   constructor(props) {
     super(props);
   
@@ -29,16 +29,16 @@ class App extends Component {
   const HomePage = () => {
     return(
       <Home 
-          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
       />
   );
   }
   const DishWithId = ({match}) => {    
     return(
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
     );
   };
    return(
@@ -46,10 +46,10 @@ class App extends Component {
       <Header/>
       <Switch>
               <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
-              <Route path="/aboutus" component={()=><About leaders={this.state.leaders}/>}/>
+              <Route path="/aboutus" component={()=><About leaders={this.props.leaders}/>}/>
               <Redirect to="/home" />
           </Switch>
       <Footer/>  
@@ -57,4 +57,4 @@ class App extends Component {
    );
  }
 }
-export default App;
+export default withRouter(connect(mapStateToProps)(Main));
