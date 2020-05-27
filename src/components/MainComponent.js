6f4b82e22e9211e5ec1d0a5bf45199096d5e90d1
ayush <ayushsaxena723@gmail.com>
2020-05-27 20:45:35 +0530

    contact-form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,7 +12,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-class App extends Component {
+class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -56,4 +56,4 @@ class App extends Component {
    );
  }
 }
-export default App;
+export default Main;
