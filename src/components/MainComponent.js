4f47436b85b274daf9553478bb56739a988bef08
ayush <ayushsaxena723@gmail.com>
2020-05-27 20:38:11 +0530

    redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,4 @@
 import React, { Component, useLayoutEffect } from 'react';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent'
 import Header from './HeaderComponent'
@@ -10,20 +6,25 @@ import Contact from './ContactComponent';
 import About from './AboutComponent'
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+}
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS
-    };  
+  
   }
- 
-
  render(){
   const HomePage = () => {
     return(
